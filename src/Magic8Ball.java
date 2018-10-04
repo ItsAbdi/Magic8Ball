98b471dafa20004085c3408302d45e590fa945ed
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-10-04 12:51:16 -0400

    testing Magic8Ball project

---
+++ src/Magic8Ball.java
@@ -0,0 +1,15 @@
+public class Magic8Ball {
+
+    public static void main(String[] args) {
+        //declare
+
+//        int firstNumber = 10
+//        int secondNumber = 69;
+//        if (firstNumber>secondNumber);
+//        System.out.println(firstNumber + " is greater than " + secondNumber);
+
+
+
+
+    }
+}
