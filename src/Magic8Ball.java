259e0a7ed7272975c7ea7d566f94f5bb48abca56
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-10-11 13:22:09 -0400

    added a play again method to the Magic8Ball project

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -9,12 +9,16 @@ public class Magic8Ball {
 //        int secondNumber = 69;
 //        if (firstNumber>secondNumber);
 //        System.out.println(firstNumber + " is greater than " + secondNumber);
+
         Scanner keyboard;
         int testing;
         String question;
+        String again;
 
         //initialization
-        keyboard = new Scanner(System.in);
+
+        do {
+            keyboard = new Scanner(System.in);
         System.out.println("what is your question? ");
         question = keyboard.nextLine();
         System.out.println(" put in a random number of your choice between 1-100.");
@@ -25,39 +29,36 @@ public class Magic8Ball {
         if ((11 <= testing) && (testing <= 20)) {
             System.out.println(" There are dark days ahead...");
         }
-        if ((21 <= testing) && (testing<=30)) {
+        if ((21 <= testing) && (testing <= 30)) {
             System.out.println(" Its a No from me dawg.");
         }
-        if ((31 <= testing) && (testing<= 40)){
+        if ((31 <= testing) && (testing <= 40)) {
             System.out.println("Yes!");
         }
-        if ((41 <= testing) && (testing<= 50)) {
+        if ((41 <= testing) && (testing <= 50)) {
             System.out.println("No!");
         }
-        if ((51 <= testing) && (testing<= 60)){
+        if ((51 <= testing) && (testing <= 60)) {
             System.out.println("Awesome!");
         }
-        if ((61 <=testing) && (testing<= 70)) {
+        if ((61 <= testing) && (testing <= 70)) {
             System.out.println("Hip-Hip Hooray");
         }
-        if ((71 <=testing) && (testing<= 80)) {
+        if ((71 <= testing) && (testing <= 80)) {
             System.out.println("Take a hike");
         }
-        if ((81 <=testing) && (testing<= 90)) {
+        if ((81 <= testing) && (testing <= 90)) {
             System.out.println("You've got luck on your side");
         }
-        if ((91 <=testing) && (testing<= 100)) {
+        if ((91 <= testing) && (testing <= 100)) {
             System.out.println("ghobe' , that's no in Klingon");
         }
 
+        System.out.println("would you like to play again (yes or no).");
+        again = keyboard.next();
 
-
-
-
-
-
-
-
+    } while (again.equals("yes"));
+            System.out.println("Goodbye");
 
 
 
